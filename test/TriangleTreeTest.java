b54bf1c88b348dfb705d57714c19af0ad9c84ebc
Luming Wu <reallumingwu@gmail.com>
2017-09-07 15:41:20 -0400

    Wrote tests and fixed List

--- test/TriangleTreeTest.java
+++ test/TriangleTreeTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package TriangleTree;
 
 import main.java.tree.TriangleTree;
 import java.util.LinkedList;
